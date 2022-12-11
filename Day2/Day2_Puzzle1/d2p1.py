b858ebf7463f49b5681032546ce7d10d6dcc5a04
Murdoch <programming.tiger.2018@gmail.com>
2022-12-11 00:35:27 -0600

    push WIP

---
+++ Day2/Day2_Puzzle1/d2p1.py
@@ -0,0 +1,43 @@
+import re
+
+moveValues = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+rulesDict = {
+    "AX": "D",
+    "AY": "W",
+    "AZ": "L",
+    "BX": "L",
+    "BY": "D",
+    "BZ": "W",
+    "CX": "W",
+    "CY": "L",
+    "CZ": "D"
+}
+
+inputFile = open("input.txt", "r")
+finalScore = 0 
+
+
+for line in inputFile:
+    if (len(line.strip()) > 0):
+        round = re.sub(r'\W+', '', line)
+
+        yourMove = round[1] 
+        result = rulesDict.get(round)
+
+        roundScore =  moveValues.get(yourMove)
+
+        if result == "W":
+            roundScore += 6
+        elif result == "D":
+            roundScore += 3
+        
+        finalScore += roundScore
+
+inputFile.close()
+
+print(finalScore)
