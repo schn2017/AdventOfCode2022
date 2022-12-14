01f3700a4db9a6454fc576a446d6d114b8167d43
Murdoch <programming.tiger.2018@gmail.com>
2022-12-14 11:53:24 -0600

    Day 6 complete

---
+++ Day6/Day6_Puzzle1/d6p1.py
@@ -0,0 +1,25 @@
+import sys
+def main():
+    inputFile = open(sys.argv[1], "r")
+    for line in inputFile:
+        sanitizedLine = line.replace("\n", "")
+        chars = []
+        processed = 0
+        for char in sanitizedLine:
+            uniqueChars = set(chars)
+            if len(uniqueChars) == 4 and chars.count(char) == 0:
+                print("Marker found " + char)
+                print(str(processed) + " chars")
+                chars = []
+                break
+
+            if len(chars) == 4:
+                chars.pop(0)
+            chars.append(char)   
+            processed += 1
+    
+            
+    inputFile.close()
+
+if __name__ == "__main__":
+    main()
