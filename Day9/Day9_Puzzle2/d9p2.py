0d244d9cbfe5aafec34f418d6dd445e1a9781fe8
Murdoch <programming.tiger.2018@gmail.com>
2022-12-18 01:48:00 -0600

    minor change

--- Day9/Day9_Puzzle2/d9p2.py
+++ Day9/Day9_Puzzle2/d9p2.py
@@ -1,6 +1,8 @@
 import sys
 def main():
     inputFile = open(sys.argv[1], "r")
+    for line in inputFile:
+        sanitizedLine = line.replace("\n", "")
     inputFile.close()
 
 if __name__ == "__main__":
