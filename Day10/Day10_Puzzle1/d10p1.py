00d76ca1374cada413e1ff25b50ea4a603b27730
Murdoch <programming.tiger.2018@gmail.com>
2022-12-18 18:09:00 -0600

    Day 10 puzzle 1 complete

--- Day10/Day10_Puzzle1/d10p1.py
+++ Day10/Day10_Puzzle1/d10p1.py
@@ -1,9 +1,58 @@
 import sys
 def main():
-    inputFile = open(sys.argv[1], "r")
-    for line in inputFile:
-        sanitizedLine = line.replace("\n", "")
-    inputFile.close()
+    cpu = CPU()
+    cpu.readInInstructions()
+    cpu.process()
 
+class Instruction:
+    def __init__(self, value, operation,  startCycle):
+        self.value = value
+        self.operation = operation
+        self.completionCycle = startCycle + 1 if operation == "addx" else startCycle
+        pass
+
+class CPU:
+    def __init__(self):
+        self.registerValue = 1
+        self.cycle = 1
+        self.instructions = []
+        self.signalStrengthChecks = [20, 60, 100, 140, 180, 220]
+        self.runningAdditions = []
+        self.signalStrengthSum = 0
+        pass
+
+    def readInInstructions(self):
+        inputFile = open(sys.argv[1], "r")
+        for line in inputFile:
+            sanitizedLine = line.replace("\n", "")
+            instruction = sanitizedLine.split(" ")
+            startingCycle = self.instructions[len(self.instructions) - 1].completionCycle + 1 if len(self.instructions) > 0 else self.cycle
+
+            if instruction[0] == "addx":
+                newInstruction = Instruction(int(instruction[1]), instruction[0], startingCycle)
+                self.instructions.append(newInstruction)
+            elif instruction[0] == "noop":
+                newInstruction = Instruction(0, instruction[0], startingCycle)
+                self.instructions.append(newInstruction)
+
+
+    def process(self):
+        while len(self.instructions) > 0:
+            self.processCycle()
+        print("The total signal strength is " + str(self.signalStrengthSum))
+
+    def processCycle(self):
+        # Add signal strength if at pre determined cycle
+        if self.cycle in self.signalStrengthChecks:
+            self.signalStrengthSum += self.cycle * self.registerValue
+            print("During the " + str(self.cycle) + "th cycle, register X has the value " + str(self.registerValue) + ", so the signal strength is " + str(self.cycle)  + " * " + str(self.registerValue) + " = " + str(self.cycle * self.registerValue))
+        
+        # Handle instruction
+        if len(self.instructions) > 0 and self.instructions[0].completionCycle == self.cycle:
+            self.runningAdditions.append(self.instructions[0].value)
+            self.registerValue += self.instructions[0].value
+            self.instructions.pop(0)
+
+        self.cycle += 1
 if __name__ == "__main__":
     main()
